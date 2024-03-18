f75ffb974bb4e97297335a8290c9dab304145b66
Leon Siqueira <leon.siqueir4@gmail.com>
2024-03-18 17:52:11 -0300

    feat: play sound on finished timer

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -2,13 +2,16 @@ import { useParticipants } from 'Participants/state'
 
 import { useTimer } from 'hooks'
 
+import { useSound } from "use-sound";
+import timeupRingtone from "../assets/audio/timeup-ringtone.mp3";
+
 import Actions from './Actions'
 
 import { Container, TimeContainer, Time } from './Timer.styles'
 
 const Timer = () => {
   const { nextParticipant } = useParticipants()
-  const { time, isRunning, start, stop, restart, getNext } = useTimer({ minutes: 300, nextParticipant })
+  const { time, isRunning, isFinished, start, stop, restart, getNext } = useTimer({ minutes: 2, nextParticipant })
 
   const normalizeTime = (time) => {
     if (String(time).length < 2) return '0' + time
@@ -16,16 +19,21 @@ const Timer = () => {
     return time
   }
 
+  const [play] = useSound(timeupRingtone, { volume: 0.8 });
+
   return (
-    <Container>
-      <TimeContainer>
-        <Time>
-          <span>{normalizeTime(Math.floor(time / 60))}</span>
-          <span>{normalizeTime(time % 60)}</span>
-        </Time>
-        <Actions {...{ isRunning, start, stop, restart, getNext }} />
-      </TimeContainer>
-    </Container>
+    <>
+      {isFinished && play()}
+      <Container>
+        <TimeContainer>
+          <Time>
+            <span>{normalizeTime(Math.floor(time / 60))}</span>
+            <span>{normalizeTime(time % 60)}</span>
+          </Time>
+          <Actions {...{ isRunning, start, stop, restart, getNext }} />
+        </TimeContainer>
+      </Container>
+    </>
   )
 }
 
